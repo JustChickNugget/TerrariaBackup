75b60f595f43a773145c615a80cef79553d523e8
JustChickNugget <belodedmatvey3@gmail.com>
2025-07-30 22:58:47 +0700

    Removed unused namespace

--- TerrariaBackup/Windows/BackupWindow.xaml.cs
+++ TerrariaBackup/Windows/BackupWindow.xaml.cs
@@ -2,7 +2,6 @@
 using TerrariaBackup.Utilities.Terraria;
 using TerrariaBackup.Models;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Windows;
 
 namespace TerrariaBackup.Windows;
